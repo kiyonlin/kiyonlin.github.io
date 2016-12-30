49302fd911cd781cb439446fbf099325c5ff581d
kiyon <kiyonlin@163.com>
2016-12-30 11:48:24 +0800

    增加粒子动画到主题配置中

--- themes/next/source/js/src/particles.js
+++ themes/next/source/js/src/particles.js
@@ -1,105 +1,134 @@
 /**
  * Created by kiyon on 2016/12/29.
  */
+
 $(document).ready(function () {
+  console.log(CONFIG.particles);
+  var _config = CONFIG.particles;
+  if (!_config.enable) {
+    return false;
+  }
+
+  _config.color = _config.color === "random" ? _config.color : "#" + _config.color;
+  _config.size_max = Math.random() * 4 + 7;
+
+  if(_config.theme === "random") {
+    _config.theme = Math.random() > 0.5 ? "snow" : "star";
+  }
+  switch (_config.theme) {
+    case "snow":
+      _config.number_value = Math.ceil(Math.random() * 25 + 32);
+      _config.shape_type = "circle";
+      _config.shape_nb_sides = "5";
+      _config.anim_enable = false;
+      _config.anim_speed = 2;
+      _config.move_speed = Math.ceil(Math.random() * 4 + 1);
+      _config.move_direction = "bottom";
+      break;
+    case "star":
+      _config.number_value = Math.ceil(Math.random() * 25 + 12);
+      _config.shape_type = "star";
+      _config.shape_nb_sides = "5";
+      _config.anim_enable = true;
+      _config.anim_speed = Math.ceil(Math.random() * 3 + 1);
+      _config.move_speed = 1;
+      _config.move_direction = "none";
+      break;
+  }
+
   particlesJS("particles-js", {
     "particles": {
       "number": {
-        "value": 32,
+        "value": _config.number_value,
         "density": {
           "enable": true,
           "value_area": 800
         }
       },
       "color": {
-        "value": "#222"
+        "value": _config.color
       },
       "shape": {
-        "type": "circle",
+        "type": _config.shape_type,
         "stroke": {
           "width": 0,
-          "color": "#222"
+          "color": "#000000"
         },
         "polygon": {
-          "nb_sides": 7
-        },
-        "image": {
-          "src": "img/github.svg",
-          "width": 100,
-          "height": 100
+          "nb_sides": _config.shape_nb_sides
         }
       },
       "opacity": {
-        "value": 0.32,
-        "random": false,
+        "value": 0.5,
+        "random": true,
         "anim": {
-          "enable": false,
+          "enable": true,
           "speed": 1,
           "opacity_min": 0.1,
           "sync": false
         }
       },
       "size": {
-        "value": 2,
+        "value": _config.size_max,
         "random": true,
         "anim": {
-          "enable": false,
-          "speed": 32,
+          "enable": _config.anim_enable,
+          "speed": _config.anim_speed,
           "size_min": 0.1,
           "sync": false
         }
       },
       "line_linked": {
-        "enable": true,
-        "distance": 120,
-        "color": "#222",
-        "opacity": 0.32,
-        "width": 1
+        "enable": false,
+        "distance": 320,
+        "color": "#ffffff",
+        "opacity": 0.4,
+        "width": 2
       },
       "move": {
         "enable": true,
-        "speed": 5,
-        "direction": "none",
-        "random": false,
+        "speed": _config.move_speed,
+        "direction": _config.move_direction,
+        "random": true,
         "straight": false,
         "out_mode": "out",
         "bounce": false,
         "attract": {
-          "enable": false,
+          "enable": true,
           "rotateX": 600,
           "rotateY": 1200
         }
       }
     },
     "interactivity": {
-      "detect_on": "canvas",
+      "detect_on": "window",
       "events": {
         "onhover": {
           "enable": true,
-          "mode": "repulse"
+          "mode": "bubble"
         },
         "onclick": {
           "enable": true,
-          "mode": "push"
+          "mode": "repulse"
         },
         "resize": true
       },
       "modes": {
         "grab": {
-          "distance": 400,
+          "distance": 250,
           "line_linked": {
-            "opacity": 1
+            "opacity": 0.5
           }
         },
         "bubble": {
-          "distance": 400,
-          "size": 32,
-          "duration": 2,
-          "opacity": 8,
+          "distance": 270,
+          "size": 4,
+          "duration": 0.3,
+          "opacity": 1,
           "speed": 3
         },
         "repulse": {
-          "distance": 200,
+          "distance": 150,
           "duration": 0.4
         },
         "push": {
